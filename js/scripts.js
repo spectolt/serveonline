1418bcc7760a4da2d451a7a6b2e24772a80be296
Liveta Valantiejūtė <l.valantiejute@gmail.com>
2020-12-22 01:05:08 +0200

    created page 58

--- js/scripts.js
+++ js/scripts.js
@@ -389,6 +389,22 @@ $(document).ready(function () {
     $(this).find(".select2-selection__arrow b").toggleClass("rotate");
   });
 
+  $("#upload-icon").change(function(e) {
+
+    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+
+        var file = e.originalEvent.srcElement.files[i];
+
+        var img = document.createElement("img");
+        var reader = new FileReader();
+        reader.onloadend = function() {
+             img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+        $("#upload-icon").after(img);
+    }
+});
+
   moveAction();
   moveOrder();
   changePadding();
--- pages/58.php
+++ pages/58.php
@@ -1,12 +1,75 @@
 <?php require_once('header.php'); ?>
 <main class="site-main site-main--aside">
 <div class="page-content page-content--flexible">
+    <section class="areas">
     <h1 class="title">Sritys</h1>
     <ul>
-        <li><a href="#">LT</a></li>
-        <li><a href="#">EN</a></li>
-        <li><a href="#">RU</a></li>
+        <li class="paragraph"><a href="#">LT</a></li>
+        <li class="paragraph"><a href="#">EN</a></li>
+        <li class="paragraph"><a href="#">RU</a></li>
     </ul>
+    <table class="areas__table">
+        <thead>
+            <tr>
+                <th>Paslaugų sritis</th>
+                <th>Spalva</th>
+                <th>Paveikslėlis .svg</th>
+                <th>Meniu pavadinimai</th>
+            </tr>
+        </thead>
+        <tr>
+            <td>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Klinikos ir estetinė medicina
+            </td>
+            <td>
+                fec994
+            </td>
+            <td>
+                <img class="areas__image" src="../img/icons/clinic.svg">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Klinikos ir estetinė medicina
+            </td>
+            <td>
+                fec994
+            </td>
+            <td>
+                <img class="areas__image" src="../img/icons/clinic.svg">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Klinikos ir estetinė medicina
+            </td>
+            <td>
+                fec994
+            </td>
+            <td>
+                <img class="areas__image" src="../img/icons/clinic.svg">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input placeholder="Pavadinimas">
+            </td>
+            <td>
+                <input placeholder="Pasirinkite spalvą">
+            </td>
+            <td>
+            <input id="upload-icon" class="custom-file-input" type="file" id="img" name="img" accept=".svg" onchange="readURL(this);">
+            </td>
+        </tr>
+    </table>
+    </section>
     <?php require_once('product-sidebar.php'); ?>
     <?php require_once('footer-menu.php'); ?>
 </div>
