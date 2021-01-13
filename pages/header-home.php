a985bb54ca6afff0cdfd8cd663c38e81ebd68a1e
Liveta Valantiejūtė <l.valantiejute@gmail.com>
2021-01-13 14:47:09 +0200

    removed search from homepage

--- pages/header-home.php
+++ pages/header-home.php
@@ -16,25 +16,9 @@
 
 <body>
     <div id="page">
-        <div class="search-container search-container--home">
-            <form class="search-panel">
-                <input type="text" name="search" placeholder="Specialistas, įstaiga, įmonių grupė..." />
-            </form>
-            <div class="search-product">
-                <div class="product__search product__search--header input-wrapper">
-                    <input type="text" id="search-product" placeholder="Paslauga. Įveskite kelis simbolius paieškai" />
-                    <button type="button" class="search-container__submit"><span>Ieškoti</span></button>
-                </div>
-            </div>
-            <button type="button" class="search-container__close"></button>
-        </div>
         <header class="site-header site-header--home site-header--show">
             <img src="../img/logo.svg" class="site-header__logo" alt="" />
             <ul class="site-header__menu site-header__menu--items">
                 <li class="site-header__item paragraph"><a href="#">Prisijungti</a></li>
-            </ul>
-            <ul class="site-header__menu site-header__menu--main">
-
-                <li class="site-header__item site-header__item--search"><a href="#"></a></li>
             </ul>
         </header>
