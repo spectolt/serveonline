c4a7fc0403e106d130969fb61e754bf3c300fe03
Liveta Valantiejūtė <l.valantiejute@gmail.com>
2021-03-01 13:25:38 +0200

    added touchpunch

--- pages/footer.php
+++ pages/footer.php
@@ -1,6 +1,7 @@
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script src="../js/jquery.ui.touch-punch.min.js"></script>
 <script src="../js/datepicker-lt.js"></script>
 <script src="../js/select2.min.js"></script>
 <script src="../js/moment-with-locales.min.js"></script>
