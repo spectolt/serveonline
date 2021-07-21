c27c3dd27110430ed696ade91368262b4fe69cbb
Specto LT <l.valantiejute@gmail.com>
2021-07-21 14:47:53 +0300

    changed text in footer

--- pages/footer-menu-homepage.php
+++ pages/footer-menu-homepage.php
@@ -3,7 +3,7 @@
     <img src="../img/serve-online-horizontal-white-logo.svg" class="site-footer__logo" />
     <ul class="site-footer__menu">
         <li class="site-footer__item paragraph"><a href="#">Privatumo politika</a></li>
-        <li class="site-footer__item paragraph"><a href="#">Teisinės nuostatos</a></li>
+        <li class="site-footer__item paragraph"><a href="#">Naudojimosi taisyklės</a></li>
         <li class="site-footer__item paragraph"><a href="#">Pagalba ir kontaktai</a></li>
     </ul>
     <div class="site-footer__langs">
--- pages/footer-menu.php
+++ pages/footer-menu.php
@@ -2,7 +2,7 @@
     <img src="../img/logo-sidebar.svg" class="site-footer__logo" />
     <ul class="site-footer__menu">
         <li class="site-footer__item paragraph"><a href="#">Privatumo politika</a></li>
-        <li class="site-footer__item paragraph"><a href="#">Teisinės nuostatos</a></li>
+        <li class="site-footer__item paragraph"><a href="#">Naudojimosi taisyklės</a></li>
         <li class="site-footer__item paragraph"><a href="#">Pagalba ir kontaktai</a></li>
     </ul>
     <div class="site-footer__langs">
